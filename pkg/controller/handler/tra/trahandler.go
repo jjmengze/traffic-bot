020b2db74c161b9b23628e84731599ba0f7613c8
MengZn <adnt587@gmail.com>
2020-06-30 14:05:04 +0800

    update

--- pkg/controller/handler/tra/trahandler.go
+++ pkg/controller/handler/tra/trahandler.go
@@ -19,14 +19,19 @@ func NewTRAHandler(handlerContext *middleware.HandlerContext, middle *middleware
 }
 
 func (e *TRAHandler) Run(stopCh <-chan struct{}) {
+	innerCh := make(chan struct{})
 	klog.Infof("Starting TRA handler ")
 	defer klog.Infof("Shutting down TRA handler ")
+	defer close(innerCh)
 
 	go func() {
 		for {
 			select {
 			case event := <-e.middle.EventCh:
 				fmt.Printf("recice %s", event)
+			case <-innerCh:
+				klog.Info("received a Interrupt signal ")
+				break
 			}
 		}
 	}()
